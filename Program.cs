a2e4877ed30627d048004847383afcab1629cacd
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-08-04 15:28:06 +0900

    Initial Program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AcidChicken.Reversid
+{
+    class Program
+    {
+        private Program(string[] args)
+        {
+        }
+
+        static async Task Main(string[] args) =>
+            new Program(args).RunAsync();
+
+        private async Task RunAsync()
+        {
+            await Task.Delay(-1);
+        }
+    }
+}
